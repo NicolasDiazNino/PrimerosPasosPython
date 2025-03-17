b9be0937c1babc183b8b69f30bdefbcac707a3fa
NicolasDiazNino <163475570+NicolasDiazNino@users.noreply.github.com.>
2025-03-16 19:06:55 -0500

    feat: :tada: Primeros pasos para aprender Python
    en este primer commit se realizo el uso de print para poder imprimir un "Hola Python", ademas de empezar a ver los tipos de datos

---
+++ PrimerosPasosPy/00_Hello.py
@@ -0,0 +1,24 @@
+# Esto es un comentario
+# Primer paso para progrmar, iniciaremos con un sencillo "Hola mundo"
+print("Hola Python")
+print('Hola Python')
+
+
+"""
+Este es un 
+comentario en 
+varias lineas
+"""
+
+'''
+Este tambien es 
+un comentario en
+varias lineas
+'''
+
+# Consulta del tipo de dato
+print(type("Hola python"))  # Tipo 'str'  
+print(type(5)) # tipo 'int'
+print(type(2.3))  # Tipo 'float'
+print(type(True))  # Tipo 'bool'
+print(type(3 + 1j))  # Tipo 'complex'
