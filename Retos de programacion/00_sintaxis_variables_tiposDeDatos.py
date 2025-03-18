60b42c384ad82baaf6400f1923b2bf3a2b5719b8
NicolasDiazNino <163475570+NicolasDiazNino@users.noreply.github.com.>
2025-03-17 22:49:56 -0500

    feat: :sparkles: cracion de la carpeta de retos e inicio de los mismos
    se creo una nueva carpeta que sirve para el inicio de los retos y ademas se creo un nuevo archivo con el primer reto

---
+++ Retos de programacion/00_sintaxis_variables_tiposDeDatos.py
@@ -0,0 +1,37 @@
+# LINK de los retos: https://retosdeprogramacion.com/roadmap
+
+# Sitio oficial del lenguaje: https://www.python.org
+
+# Esto es un tipo de comentario de una sola linea
+# solo funciona si se coloca el nuemra antes
+
+'''
+Este es otro tipo de comentrio, se usa
+mas que todo para cometar un texto con 
+mas de una linea
+'''
+
+'''
+No existen constantes en el 
+lenguaje de Python
+pero si variables
+'''
+
+my_variable = "Mi variable"
+my_new_variable = "Mi nueva variable"
+
+MY_CONSTANT = "Mi constante"
+
+my_int = 19
+my_float = 1.75
+my_bool = True
+my_bool = False
+my_string = "Mi cadena de texto"
+my_other_string = "Mi otra cadena de texto"
+
+print("Hola Python")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
